e3ecd40b715b1a80f9e6d6a42f8cf73172f0e6ba
glplay <glplay.iptv@gmail.com>
2025-05-22 14:44:39 -0300

    Update webhook_sendpulse_jwt.py

--- webhook_sendpulse_jwt.py
+++ webhook_sendpulse_jwt.py
@@ -49,4 +49,8 @@ def handle_webhook():
         }), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=True)
+
